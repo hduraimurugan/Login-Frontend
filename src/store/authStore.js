825d3e00315ef6d1dec5a19f821dae6dd2a3d91b
Duraimurugan-H <hduraimurugan@gmail.com>
2024-09-14 05:47:28 +0530

    API url

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+// const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
-// const API_URL ="http://localhost:5000/api/auth";
+const API_URL ="https://login-backend-2aaw.onrender.com/";
 
 axios.defaults.withCredentials = true;
 
