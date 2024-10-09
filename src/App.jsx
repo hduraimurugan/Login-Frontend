f75419242dd983b454cc5f148e008d7190bf5b93
Duraimurugan-H <hduraimurugan@gmail.com>
2024-10-09 18:57:19 +0530

    route bug

--- src/App.jsx
+++ src/App.jsx
@@ -70,9 +70,9 @@ function App() {
 				<Route
 					path='/signup'
 					element={
-						<RedirectAuthenticatedUser>
+					
 							<SignUpPage />
-						</RedirectAuthenticatedUser>
+						
 					}
 				/>
 				<Route
